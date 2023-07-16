fb6795a4918329141f7eb78702d918e16eb8c91c
Talha Idrees <talhaidrees116@gmail.com>
2023-07-16 19:17:37 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React e870e22807cb
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+/*
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={ProductList} />
+        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/checkout" component={Checkout} />
+        <Route component={NotFound} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;*/
