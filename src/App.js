a0ec03a77dba4c888c9ec90ef787f2524bc0385e
Talha Idrees <talhaidrees116@gmail.com>
2023-07-19 05:11:09 +0500

    change app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React e870e22807cb
+          Learn React from me
         </a>
       </header>
     </div>
